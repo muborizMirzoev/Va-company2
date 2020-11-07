d825afadab50db2a44463177e39fa7519567e5a2
muborizMirzoev <muboriz10@gmail.com>
2020-11-07 18:58:34 +0700

    feat: add responsive nav

---
+++ js/sript.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const mobileToggle = document.querySelector('.mobile-toggle');
+
+  mobileToggle.addEventListener("click", function(){
+    this.classList.toggle('active');
+  });
+
+
+
+
+
+
+console.log(body.scrollHeight);
+
+
+// $(window).scroll(function(e){
+//     if(header.offset().top !== 0){
+//         if(!header.hasClass('shadow')){
+//             header.addClass('shadow');
+//         }
+//     }else{
+//         header.removeClass('shadow');
+//     }
+// });
+});
+
